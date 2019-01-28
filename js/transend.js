6e2143e5ac436cc6764ec636eacae2fc11fc3230
Dshtc <47029190+Dshtc@users.noreply.github.com>
2019-01-29 01:16:16 +0200

    Add files via upload

--- js/transend.js
+++ js/transend.js
@@ -1,3 +1,7 @@
+viz.config.set('websocket','https://rpc.viz.lexai.host/'); 
+viz.config.set('address_prefix','VIZ');
+viz.config.set('chain_id','2040effda178d4fffff5eab7a915d4019879f5205cc5392e4bcced2b6edda0cd');
+
 var BUTT = document.querySelector("a.trans");
 BUTT.addEventListener("click", function( event ) {
 var  wif= document.getElementById("wif").value;
@@ -8,12 +12,8 @@ var  memo= document.getElementById("memo").value;
 var bbb = Math.round(mone*1000)/1000;;
 var vvv= bbb.toFixed(3)
 
-viz.config.set('websocket','https://rpc.viz.lexai.host/'); 
-viz.config.set('address_prefix','VIZ');
-viz.config.set('chain_id','2040effda178d4fffff5eab7a915d4019879f5205cc5392e4bcced2b6edda0cd');
-
 viz.broadcast.transfer(wif, sender, pol, vvv + ' VIZ', memo, function(err, result) {
-  console.log(err, result);
+  
    if(result) { 
  console.log(result);
   document.getElementById('urra').innerHTML = "<div class='yeah'><h2>УСПЕШНО!</h2></div>";
@@ -23,5 +23,54 @@ viz.broadcast.transfer(wif, sender, pol, vvv + ' VIZ', memo, function(err, resul
     setTimeout(function(err, result) { 
 	   document.getElementById('urra').innerHTML = "<div class='err'></div>";                        
 		}, 7000 ); 
-} );
+});
+
+});
+
+var BUTTS = document.querySelector("a.transs");
+BUTTS.addEventListener("click", function( event ) {
+var  wifs= document.getElementById("wifs").value;
+var  senders= document.getElementById("senders").value.toLocaleLowerCase().trim();
+var  pols= document.getElementById("pols").value.toLocaleLowerCase().trim();
+var  mones= document.getElementById("mones").value;
+var bbb = Math.round(mones*1000)/1000;
+var vvv= bbb.toFixed(6)
+
+viz.broadcast.delegateVestingShares(wifs, senders, pols, vvv+' SHARES', function(err, result) {
+  console.log(err, result);
+   if(result) { 
+ 
+  document.getElementById('urras').innerHTML = "<div class='yeah'><h2>УСПЕШНО!</h2></div>";
+   } else{ 
+   document.getElementById('urras').innerHTML = "<div class='nono'><h2>ОШИБКА!</h2></div>";
+  }
+    setTimeout(function(err, result) { 
+	   document.getElementById('urras').innerHTML = "<div class='err'></div>";                        
+		}, 7000 ); 
+});
+
+});
+
+
+var BUTTSS = document.querySelector("a.transss");
+BUTTSS.addEventListener("click", function( event ) {
+var  wifss= document.getElementById("wifss").value;
+var  senderss= document.getElementById("senderss").value.toLocaleLowerCase().trim();
+var  moness= document.getElementById("moness").value;
+var bbb = Math.round(moness*1000)/1000;
+var vvv= bbb.toFixed(3)
+
+viz.broadcast.transferToVesting(wifss, senderss, senderss, vvv+' VIZ', function(err, result) {
+  console.log(err, result);
+   if(result) { 
+ 
+  document.getElementById('urrass').innerHTML = "<div class='yeah'><h2>УСПЕШНО!</h2></div>";
+   } else{ 
+   document.getElementById('urrass').innerHTML = "<div class='nono'><h2>ОШИБКА!</h2></div>";
+  }
+    setTimeout(function(err, result) { 
+	   document.getElementById('urrass').innerHTML = "<div class='err'></div>";                        
+		}, 7000 ); 
+});
+
 });
